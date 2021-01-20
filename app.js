b7b4a0449f81b635b269238e104866503ee05db9
umairsolangi391 <76113491+umairsolangi391@users.noreply.github.com>
2021-01-20 21:24:56 +0500

    Tabel

---
+++ app.js
@@ -0,0 +1,45 @@
+// function add(){
+//     var a = 45;
+//     var b = 33;
+//     var c = 33;
+//     var d = a+b+c;
+//     alert(d)
+// }
+// add()
+
+// function greeting(greet){
+// alert(greet)
+// }
+// greeting("Hellow World")
+
+// function add(){
+//     var a = 44;
+//     var b = 43;
+//     var c = a+b;
+//     return(c);
+// }
+// var d = add()
+// alert(d)
+
+// var check = prompt("Enter Your Gender")
+// switch(check){
+//     case 1: 
+//     day = "male"
+//     alert("You are genious")
+//     break;
+//     case 2:
+//     day = "female"
+//         alert("You are super genious")
+//     break;
+//     default:
+//         alert("who you are you data isn't exist")
+// }
+var loc = document.getElementsByClassName("tabel-location")
+var tabel = prompt("Enter Number for Multiplication")
+for(var i = 1; i<=10; i++){
+    document.write(tabel + " " + "x" + " " + i + " " + " =" + " " + i*tabel + "<br />")
+}
+// var input = document.getElementById("input");
+// function ismail(){
+//     console.log(input.value)
+// }
